b0d8ad10a274066becd34bf9da32a57ff49cee72
zyx670618 <742531744@qq.com>
2019-10-22 11:03:21 +0800

    first commit

---
+++ app/src/main/java/com/example/ml/tinkertest/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.ml.tinkertest;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loadPatchButton = (Button) findViewById(R.id.loadPatch);
+
+        loadPatchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk");
+            }
+        });
+
+    }
+
+}
