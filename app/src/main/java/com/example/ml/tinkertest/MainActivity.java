2da1fa861156138b9a41d39fbd51123dcd0e7c23
zyx670618 <742531744@qq.com>
2019-10-23 17:50:02 +0800

    测试补丁包

--- app/src/main/java/com/example/ml/tinkertest/MainActivity.java
+++ app/src/main/java/com/example/ml/tinkertest/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         loadPatchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk");
+                TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), Environment.getExternalStorageDirectory().getAbsolutePath() + "/apkDownload/patch_signed_7zip.apk");
             }
         });
 
