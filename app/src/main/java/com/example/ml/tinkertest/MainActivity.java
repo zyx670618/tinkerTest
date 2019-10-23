62ff90b97360581ef8d48b7f057e994cc92992c9
zyx670618 <742531744@qq.com>
2019-10-23 09:56:37 +0800

    实现下载apk及适配Android 6.0以上版本读写权限、打开文件权限

--- app/src/main/java/com/example/ml/tinkertest/MainActivity.java
+++ app/src/main/java/com/example/ml/tinkertest/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.ml.tinkertest;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.tencent.tinker.lib.tinker.TinkerInstaller;
 
@@ -16,16 +31,44 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    //  上下文
+    private Context context;
+    //  进度条
+    private ProgressBar mProgressBar;
+    //  对话框
+    private Dialog mDownloadDialog;
+    //  判断是否停止
+    private boolean mIsCancel = false;
+    //  进度
+    private int mProgress;
+    //  文件保存路径
+    private String mSavePath;
+    //  版本名称
+    private String mVersion_name="1.0";
+    //  请求链接
+    private String url ="https://download.dgstaticresources.net/fusion/android/app-c6-release.apk";
+
+    public  int myRequestCode = 100;
+
+    String[] permissions = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    // 声明一个集合，在后面的代码中用来存储用户拒绝授权的权
+    List<String> mPermissionList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
 
         Button loadPatchButton = (Button) findViewById(R.id.loadPatch);
-
         loadPatchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +76,197 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button download = (Button)findViewById(R.id.download);
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIsCancel=false;
+                
+                // 代码编译时的目标 sdk
+                int targetSdkVersion = MainActivity.this.getApplicationInfo().targetSdkVersion;
+                // 当前运行设备的sdk的版本号（即当前手机系统的版本号）
+                int devicesSdkVersion = Build.VERSION.SDK_INT;
+                // 安卓6.0对应的sdk是23，即这个值就是23
+                int androidSdk23 = Build.VERSION_CODES.M;
+                Log.e("111",targetSdkVersion+">>>"+devicesSdkVersion+">>>"+androidSdk23);
+                // 同时满足这两个条件，需要动态申请权限（注意很多资料都是只判断了一个条件）
+                if (targetSdkVersion >= androidSdk23 && devicesSdkVersion >= androidSdk23) {
+                    Log.e("111","6.0动态授权");
+                    checkPermissions();
+                }else{
+                    Log.e("111","主清单授权");
+                    //展示对话框
+                    showDownloadDialog();
+                }
+            }
+        });
+
+    }
+
+    private void checkPermissions(){
+        for (int i = 0; i < permissions.length; i++) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, permissions[i]) != PackageManager.PERMISSION_GRANTED) {
+                mPermissionList.add(permissions[i]);
+            }
+        }
+        if (mPermissionList.isEmpty()) {
+            //未授予的权限为空，表示都授予了
+            Toast.makeText(MainActivity.this, "已经授权", Toast.LENGTH_LONG).show();
+            //展示对话框
+            showDownloadDialog();
+        } else {
+            //请求权限方法
+            String[] permissions = mPermissionList.toArray(new String[mPermissionList.size()]);
+            //请求权限
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, myRequestCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == myRequestCode) {
+            for (int i = 0; i < grantResults.length; i++) {
+                if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {  //点击禁止
+                    //判断是否勾选禁止后不再询问
+                    boolean showRequestPermission = ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, permissions[i]);
+                    if (showRequestPermission) {
+                        Toast.makeText(MainActivity.this, "权限未申请", Toast.LENGTH_SHORT).show();
+                    }
+                }else {  //点击允许
+                    //展示对话框
+                    showDownloadDialog();
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    /*
+     * 显示正在下载对话框
+     */
+    protected void showDownloadDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("下载中");
+        View view = LayoutInflater.from(context).inflate(R.layout.dialog_progress, null);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.id_progress);
+        builder.setView(view);
+
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 隐藏当前对话框
+                dialog.dismiss();
+                // 设置下载状态为取消
+                mIsCancel = true;
+            }
+        });
+
+        mDownloadDialog = builder.create();
+        mDownloadDialog.show();
+
+        // 下载文件
+        downloadAPK();
+    }
+    /*
+     * 开启新线程下载apk文件
+     */
+    private void downloadAPK() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+                        String sdPath = Environment.getExternalStorageDirectory() + "/";
+                       //文件保存路径
+                        mSavePath = sdPath + "apkDownload";
+
+                        File dir = new File(mSavePath);
+                        if (!dir.exists()){
+                            dir.mkdir();
+                        }
+                        // 下载文件
+                        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+                        conn.connect();
+                        InputStream is = conn.getInputStream();
+                        int length = conn.getContentLength();
+
+                        File apkFile = new File(mSavePath, mVersion_name);
+                        FileOutputStream fos = new FileOutputStream(apkFile);
+
+                        int count = 0;
+                        byte[] buffer = new byte[1024];
+                        while (!mIsCancel){
+                            int numread = is.read(buffer);
+                            count += numread;
+                            // 计算进度条的当前位置
+                            mProgress = (int) (((float)count/length) * 100);
+                            // 更新进度条
+                            mUpdateProgressHandler.sendEmptyMessage(1);
+
+                            // 下载完成
+                            if (numread < 0){
+                                mUpdateProgressHandler.sendEmptyMessage(2);
+                                break;
+                            }
+                            fos.write(buffer, 0, numread);
+                        }
+                        fos.close();
+                        is.close();
+                    }
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * 接收下载完成消息
+     */
+    private Handler mUpdateProgressHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case 1:
+                    // 设置进度条
+                    mProgressBar.setProgress(mProgress);
+                    break;
+                case 2:
+                    // 隐藏当前下载对话框
+                    mDownloadDialog.dismiss();
+                    // 安装 APK 文件
+                    installAPK();
+            }
+        };
+    };
+
+
+    /*
+     * 下载到本地后执行安装
+     */
+    protected void installAPK() {
+        File apkFile = new File(mSavePath, mVersion_name);
+        if (!apkFile.exists()){
+            return;
+        }
+
+        /**
+         * 1、首先我们对Android N及以上做判断；
+         * 2、然后添加flags，表明我们要被授予什么样的临时权限
+         * 3、以前我们直接 Uri.fromFile(apkFile)构建出一个Uri,现在我们使用FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + “.fileProvider”, apkFile);
+         * 4、BuildConfig.APPLICATION_ID直接是应用的包名
+         */
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        //判断是否是AndroidN以及更高的版本
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            Uri contentUri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".fileProvider", apkFile);
+            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
+        } else {
+            intent.setDataAndType(Uri.fromFile(apkFile), "application/vnd.android.package-archive");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        }
+        startActivity(intent);
     }
 
 }
